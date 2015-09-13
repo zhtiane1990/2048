231c7abe6cbf8361e89dfc5e4d5dfc441c783dd6
e <zhtiane_1990@126.com>
2015-09-13 14:50:05 +0800

    mainView background add

--- 2048/MainViewController.swift
+++ 2048/MainViewController.swift
@@ -10,26 +10,47 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    //
+    var dimension: Int = 4
+    
+    //
+    var maxnumber: Int = 2048
+    
+    var width: CGFloat = 50
+    
+    //
+    var padding: CGFloat = 6
+    
+    var backgrounds:Array<UIView> = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        setupBackground()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func setupBackground() {
+        var x: CGFloat = 30
+        var y: CGFloat = 150
+        
+        for i in 0..<dimension
+        {
+            y = 150
+            for j in 0..<dimension
+            {
+                var background = UIView(frame: CGRectMake(x, y, width, width))
+                
+                background.backgroundColor = UIColor.darkGrayColor()
+                
+                self.view.addSubview(background)
+                
+                backgrounds.append(background)
+                
+                y += padding + width
+                
+            }
+            x += padding + width
+            
+        }
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
