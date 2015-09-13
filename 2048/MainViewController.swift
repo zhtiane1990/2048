5714aed8860875a3d074a419a93c97e0dd67b12d
e <zhtiane_1990@126.com>
2015-09-13 13:09:48 +0800

    add  2 viewController

---
+++ 2048/MainViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MainViewController.swift
+//  2048
+//
+//  Created by e on 15/9/13.
+//  Copyright (c) 2015年 e. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
