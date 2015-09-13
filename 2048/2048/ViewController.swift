dc432f7ce71aa4b918c232f4eec4922de27b69be
e <zhtiane_1990@126.com>
2015-09-13 11:26:52 +0800

    first commit

---
+++ 2048/2048/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  2048
+//
+//  Created by e on 15/9/13.
+//  Copyright (c) 2015年 e. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBAction func startGame(sender: UIButton) {
+        let alertView = UIAlertView()
+        alertView.title = "start"
+        alertView.message = "are you ready?"
+        alertView.addButtonWithTitle("go!")
+        alertView.show()
+    }
+}
+
